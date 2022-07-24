e9ab85813e6747520f35159228c72e05ab09f7f3
Araz Shahinian <arshahinian@gmail.com>
2022-07-24 17:43:35 -0400

    working on it

--- petfax/pet.py
+++ petfax/pet.py
@@ -4,8 +4,13 @@ import json
 pets = json.load(open('pets.json'))
 print(pets)
 
-bp = Blueprint('pet', __name__, url_prefix="/pets")
+bp = Blueprint('pet', __name__, url_prefix="/main")
 
 @bp.route('/')
 def index(): 
-    return render_template('index.html', pets=pets)
+    return render_template('/main/index.html', pets=pets)
+
+@bp.route('/<int:id>')
+def showPetFacts(id): 
+    pet = pets[id - 1]
+    return render_template('/main/showPetFacts.html', pet=pet)
